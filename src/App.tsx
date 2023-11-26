47a44ec64caec20ebcc8ca55288c5242e9c4d91c
amanatsu027 <amanatsu027@gmail.com>
2023-11-26 17:42:27 +0900

    noeko

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import { Component } from "solid-js";
-import { AiOutlineMessage } from "solid-icons/ai";
-import { FiTrash } from "solid-icons/fi";
-import { FaRegularImage, FaRegularHeart } from "solid-icons/fa";
+import { Component, createSignal, createUniqueId, For } from "solid-js";
+import { FaRegularImage } from "solid-icons/fa";
 import MainLayout from "./components/layouts/Main";
+import GlidePost from "./components/glides/GlidePost";
+import { Glide } from "./types/Glide";
 
 const App: Component = () => {
+  const [content, setContent] = createSignal("");
+  const [glides, setGlides] = createSignal<Glide[]>([]);
+
+  const createGlide = () => {
+    const glide = {
+      id: createUniqueId(),
+      content: content(),
+      user: {
+        nickName: "Filip99",
+        avatar:
+          "https://www.pinclipart.com/picdir/middle/133-1331433_free-user-avatar-icons-happy-flat-design-png.png",
+      },
+      likesCount: 0,
+      subglidesCount: 0,
+      date: new Date(),
+    };
+
+    setGlides([glide, ...glides()]);
+    setContent("");
+  };
+
   return (
     <MainLayout>
       {/* HOME PAGE START */}
@@ -21,6 +42,10 @@ const App: Component = () => {
         <div class="flex-it flex-grow">
           <div class="flex-it">
             <textarea
+              value={content()}
+              onInput={(event) => {
+                setContent(event.currentTarget.value);
+              }}
               name="content"
               rows="1"
               id="glide"
@@ -37,10 +62,11 @@ const App: Component = () => {
             </div>
             <div class="flex-it w-32 mt-3 cursor-pointer">
               <button
+                onClick={createGlide}
                 type="button"
                 class="
-                            disabled:cursor-not-allowed disabled:bg-gray-400
-                            bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition duration-200"
+                  disabled:cursor-not-allowed disabled:bg-gray-400
+                  bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition duration-200"
               >
                 <div class="flex-it flex-row text-sm font-bold text-white items-start justify-center">
                   <span>Glide It</span>
@@ -52,47 +78,7 @@ const App: Component = () => {
         {/* MESSENGER END */}
       </div>
       <div class="h-px bg-gray-700 my-1" />
-      {/* GLIDE POST START */}
-      <div class="flex-it p-4 border-b-1 border-solid border-gray-700">
-        <div class="flex-it flex-row">
-          <div class="flex-it mr-4">
-            <div class="w-12 h-12 overflow-visible cursor-pointer transition duration-200 hover:opacity-80">
-              <img
-                class="rounded-full"
-                src="https://www.pinclipart.com/picdir/middle/133-1331433_free-user-avatar-icons-happy-flat-design-png.png"
-              ></img>
-            </div>
-          </div>
-          <article class="flex-it flex-grow flex-shrink cursor-pointer">
-            <div class="flex-it justify-center flex-grow mb-1">
-              <div class="flex-it justify-between flex-row w-full">
-                <div>
-                  <span class="font-bold">Filip99</span>
-                  <span class="mx-2">&#8226;</span>
-                  <span class="text-gray-400">2h</span>
-                </div>
-                <div class="text-gray-400 cursor-pointer transition hover:text-red-400">
-                  <FiTrash size={16} />
-                </div>
-              </div>
-            </div>
-            <div class="flex-it flex-row flex-grow-0 items-center mb-2">
-              <div class="flex-it mr-3 mb-3 w-full">My First Post</div>
-            </div>
-            <div class="flex-it flex-row flex-grow text-gray-400">
-              <div class="flex-it flex-row items-center cursor-pointer mr-5 transition hover:text-blue-400">
-                <AiOutlineMessage size={18} />
-                <span class="text-xs ml-3">321</span>
-              </div>
-              <div class="flex-it flex-row items-center cursor-pointer transition hover:text-pink-400">
-                <FaRegularHeart size={18} />
-                <span class="text-xs ml-3">123</span>
-              </div>
-            </div>
-          </article>
-        </div>
-      </div>
-      {/* GLIDE POST END */}
+      <For each={glides()}>{(glide) => <GlidePost glide={glide} />}</For>
       {/* HOME PAGE END */}
     </MainLayout>
   );
